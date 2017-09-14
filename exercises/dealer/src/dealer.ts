41abbbd9376c8c3f2b1dd5130cce6700aa2737b6
Lisa Huang <lisa.yc.huang@gmail.com>
2017-09-14 14:31:15 -0500

    Updated solution for Exercise 3 - card dealer

--- exercises/dealer/src/dealer.ts
+++ exercises/dealer/src/dealer.ts
@@ -12,6 +12,49 @@ function shuffleArray(a: any[]) {
   }
 }
 
-export class Dealer {
+export enum Suit {
+  Clubs, Diamonds, Hearts, Spades
+}
 
+export enum CardNumber {
+  Ace, Two, Three, Four, Five, Six, Seven,
+  Eight, Nine, Ten, Jack, Queen, King
+}
+
+type Card = [Suit, CardNumber];
+
+function createDeck(): Card[]{
+  let cards: Card[] = [];
+  for (let s = 0; s < Object.keys(Suit).length; s+=2){
+    for (let c =0; c < Object.keys(CardNumber).length; c+=2 ){
+      cards.push([s/2, c/2]);
+    }
+  }
+  return cards;
+};
+
+export class Dealer {
+  cards: Card[] = [];
+  constructor() {
+    this.cards = createDeck();
+    shuffleArray(this.cards);
+  }
+
+  // deals 5 cards [Suit(0-3), Number(0-12)]
+  dealHand(numCards: number):Card[] {
+    if numCards > this.getLength() throw new Error('too many cards');
+    if numCards < 0 throw new Error('Please give me your cards');
+    
+    return this.cards.splice(this.getLength() - numCards, numCards);
+  };
+
+  //  number of cards left in the deck
+  getLength(): number {
+    return this.cards.length;
+  }
+
+  readCard(card: Card): string{
+    let [suit, CardNumber] = card;
+    return `${card[CardNumber]} of ${card[suit]}`
+  }
 }
