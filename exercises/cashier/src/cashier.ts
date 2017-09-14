d01508e1295c1e53af9755b8a131c246e15e6468
Lisa Huang <lisa.yc.huang@gmail.com>
2017-09-14 15:45:32 -0500

    Updated solution for exercise 4 - Cashier

--- exercises/cashier/src/cashier.ts
+++ exercises/cashier/src/cashier.ts
@@ -1,3 +1,38 @@
+interface CartItem{
+  name: string;
+  price: number;
+  qty: number;
+}
+
+interface CartAPI{
+  length: number;
+  total: number;
+  addItem: (cartItem: CartItem) => CartAPI;
+  add: (name:string, price: number, qty?: number) => CartAPI;
+}
+
 export function cashier() {
-  return {};
+  let items: CartItem[] = [];
+  return {
+    get length() {
+      return items.reduce((tot, item) => {
+        return tot + item.qty;
+      }, 0);
+    },
+    get total() {
+      return items.reduce((tot, item) => {
+        return Math.round(tot + (item.price * item.qty) * 100);
+      }, 0) * 0.01;
+    }, 
+    add(name, price, qty=1): CartAPI{
+      items.push({
+        name, price, qty
+      });
+      return this;
+    },
+    addItem(item):CartAPI{
+      items.push(item);
+      return this;
+    }
+  };
 }
